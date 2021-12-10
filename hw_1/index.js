59e7c74c4de42761cbe678dc63894d890d276b06
Alex Martynov <lovealex574@gmail.com>
2021-12-10 14:17:19 +0300

    Finaly hw_1

--- hw_1/index.js
+++ hw_1/index.js
@@ -1,34 +1,31 @@
-let colors = require("colors/safe")
-const elem = Math.floor(Math.random() * 10)
-let numbers = []
+let colors = require("colors/safe");
+const elem = Math.floor(Math.random() * 10);
+let numbers = [];
 let makeNum = (elem) => {
-    
     for(let i = 0; i < elem; i++) {
         let randomNum = Math.floor(Math.random() * 10)
         numbers.push(randomNum)
-        test(numbers[i], numbers)
+        checkHandler(i, numbers)
     }
-}
+};
 
-makeNum(elem)
+makeNum(elem);
 
-function test (key, numbers) {
-    const firstNum = numbers[0];
-    const secondNum = numbers[1];
-    const therdNum = numbers[2];
+function checkHandler (elem, numbers) {
+    let key = `numbers[${elem}]`;
     switch (key) {
-        case firstNum:
-            console.log(colors.green(firstNum));
+        case `numbers[${0}]`:
+            console.log(colors.green(numbers[0]));
             break;
-        case secondNum:
-            console.log(colors.yellow(secondNum));
+        case `numbers[${1}]`:
+            console.log(colors.yellow(numbers[1]));
             break;
-        case therdNum:
-            console.log(colors.red(therdNum));
+        case `numbers[${2}]`:
+            console.log(colors.red(numbers[2]));
             break;
 
         default:
-            console.log("----");
+            console.log(colors.blue(numbers[elem]));
             break;
     }
-}
+};
