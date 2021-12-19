dcb15562ceacbcce774390811afcd62421d19cf5
Alex Martynov <lovealex574@gmail.com>
2021-12-19 14:58:05 +0300

    addClass component Timer

--- hw_2/index.js
+++ hw_2/index.js
@@ -1,4 +1,7 @@
 //---- Формат Д М Г ----//
+//---- Формат Ч М С ----//
+
+//---- Объявление переменных ----//
 const moment = require("moment");
 const { clearInterval } = require("timers");
 const args = process.argv.slice(2);
@@ -6,18 +9,33 @@ const time = moment().format("h:mm:ss").split(":")
 const hours = time[0]
 const minutes = time[1]
 const seconds = time[2]
+const createTimer = (time) => {new Timer(time)}
 
 // console.log(moment().format());
 // console.log(moment().format("YYYY, MMMM, DDDD").split("-")); // Год месяц день
 // console.log(moment().format("h:mm:ss").split(":")); // Часы минуты секунды
-// console.log(time);
+console.log(time);
 // console.log(hours, minutes, seconds);
 
-/**
- * 
- * @param {*} param Параметр обратного отсчета
- * @returns 
- */
+//---- Основная логика ----//
+class Timer{
+    constructor(elem){
+        this.elem = elem;
+        this.newTimer()
+    }
+    newTimer = () => {setInterval(() => {
+        if(this.elem > 0) {
+            this.elem--
+            console.log(this.elem);
+        } else {
+            clearInterval(this.newTimer)
+        }
+        }, 1000);
+    }
+}
+
+time.forEach((elem) => {createTimer(elem)})
+
 // const newTimer = (param) => {setInterval(() => {
 //         if(param > 0) {
 //             param--
@@ -30,22 +48,3 @@ const seconds = time[2]
 // }
 
 // time.forEach((elem) => {newTimer(elem)})
-
-console.log('Record 1');
-
-setTimeout(() => {
-  console.log('Record 2');
-  Promise.resolve().then(() => {
-    setTimeout(() => {
-        console.log('Record 3');
-    Promise.resolve().then(() => {
-      console.log('Record 4');
-      });       
-    });
-  });       
-});
-
-console.log('Record 5');
-
-Promise.resolve().then(() => Promise.resolve().then(() => console.log('Record 6')));
-
