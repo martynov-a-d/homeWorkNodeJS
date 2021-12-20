c6b24206e1323e545ef6d183dc454b7b26e2536b
Alex Martynov <lovealex574@gmail.com>
2021-12-20 11:26:38 +0300

    addEventEmitter in logic

--- hw_2/index.js
+++ hw_2/index.js
@@ -6,27 +6,50 @@ const moment = require("moment");
 const { clearInterval } = require("timers");
 const args = process.argv.slice(2);
 const time = moment().format("h:mm:ss").split(":")
-const hours = time[0]
-const minutes = time[1]
-const seconds = time[2]
+const hours = {param: time[0], type: "hours"}
+const minutes = {param: time[1], type: "minutes"}
+const seconds = {param: time[2], type: "seconds"}
 const createTimer = (time) => {new Timer(time)}
+const EventEmitter = require("events")
+let arrayData = []
 
 // console.log(moment().format());
 // console.log(moment().format("YYYY, MMMM, DDDD").split("-")); // Год месяц день
 // console.log(moment().format("h:mm:ss").split(":")); // Часы минуты секунды
-console.log(time);
+// console.log(time);
 // console.log(hours, minutes, seconds);
 
 //---- Основная логика ----//
+arrayData.push(hours, minutes, seconds)
+
 class Timer{
     constructor(elem){
-        this.elem = elem;
-        this.newTimer()
+        this.elem = elem.param;
+        this.type = elem.type;
+        this.switchHandler(this.type);
+    }
+    switchHandler(key) {
+        switch (key) {
+            case "hours":      
+                this.newTimer(this.elem, this.type);
+                break;
+
+            case "minutes":
+                this.newTimer(this.elem, this.type); 
+                break;
+
+            case "seconds":
+                this.newTimer(this.elem, this.type);
+                break;
+
+            default:
+                break;
+        }
     }
     newTimer = () => {setInterval(() => {
         if(this.elem > 0) {
             this.elem--
-            console.log(this.elem);
+            console.log(this.elem, this.type);
         } else {
             clearInterval(this.newTimer)
         }
@@ -34,17 +57,12 @@ class Timer{
     }
 }
 
-time.forEach((elem) => {createTimer(elem)})
+class NewEmitter extends EventEmitter {}
 
-// const newTimer = (param) => {setInterval(() => {
-//         if(param > 0) {
-//             param--
-//             console.log(param);
-//         } else {
-//             clearInterval(newTimer)
-//         }
-//     }, 1000);
-//     return false
-// }
+const emitter = new NewEmitter
 
-// time.forEach((elem) => {newTimer(elem)})
+emitter.on("create", (elem) => createTimer(elem))
+
+arrayData.forEach((elem) => {
+    emitter.emit("create", elem)
+})
